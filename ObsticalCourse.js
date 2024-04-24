6a20ab4ae682cdcc7cae26f83950481a6337b105
Shelton18 <SheltonLaDanien@gmail.com>
2024-04-24 14:50:00 -0400

    Obstacle Course #1 Check point 1

--- ObsticalCourse.js
+++ ObsticalCourse.js
@@ -6,3 +6,12 @@
 //Program URL: https://edu.sphero.com/program/16752737/edit
 
 
+//Check Point 1
+//Programmer 1: Preston Simmons
+async function startProgram() {
+	await roll((getHeading() + 0), 60, 1.55);
+	await delay(1);
+
+
+
+}
