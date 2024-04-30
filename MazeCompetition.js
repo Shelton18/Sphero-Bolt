af7ecbe1539dba515f40dac81ed852614c502a8c
Shelton18 <SheltonLaDanien@gmail.com>
2024-04-30 13:28:50 -0400

    Added information and start program js

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: LaDanien Shelton 
+// Team Member:Preston Simmons
+//Bolt ID:6B78
+//Date: 4.16.2024
+//Program:Obstacle Course
+//Program URL:https://edu.sphero.com/program/16788351/edit
+
+async function startProgram() {
+	// Write code here
+}
