57cd47753ee66e2250e6a3717d5b0f05c377eb1f
Shelton18 <SheltonLaDanien@gmail.com>
2024-05-01 13:41:15 -0400

    Programmer: Preston Simmons - Start Text & Audio Checkpoint Commit - Date

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -1,10 +1,50 @@
-// Programmer: LaDanien Shelton 
-// Team Member:Preston Simmons
-//Bolt ID:6B78
-//Date: 4.16.2024
-//Program:Obstacle Course
-//Program URL:https://edu.sphero.com/program/16788351/edit
+
+// Programmer: LaDanien Shelton  
+// Team Member:  Preston Simmons 
+//Bolt ID: 6B78
+//Date: 4.30.2024
+//Program: Maze Competition
+//Program URL:https://edu.sphero.com/program/16788335/edit
 
 async function startProgram() {
-	// Write code here
+	// Programmer: Preston Simmons - Start Text & Audio Checkpoint Commit - Date
+	   await scrollMatrixText('Start!', {R: 66, G; 56, b: 255}, 90, true)
+	   await delay(1)
+	   awati speak("Start", true);
+
+	// Programmer - Blue LED Checkpoint Commit - Date:
+
+
+
+	//Programmer:           - Sound One Checkpoint Commit - Date:
+
+
+
+	// Programmer 			- Red LED Checkpoint Commit - Date:
+
+
+
+
+	//Programmer:           - Sound Two Checkpoint Commit - Date:
+
+
+
+
+	// Programmer 			- Green LED Checkpoint Commit - Date:
+
+
+
+
+	// Programmer 			- Purple LED & Sound Three Checkpoint Commit - Date:
+
+
+
+
+	// Programmer			- Finish Text & Audio Checkpoint Commit - Date
+
 }
+	
+
+
+
+	
