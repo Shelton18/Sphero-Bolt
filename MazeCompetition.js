76f539df47398297a5c3e29a01f627b788e476bf
Shelton18 <SheltonLaDanien@gmail.com>
2024-05-01 13:42:18 -0400

    // Programmer:LaDanien Shelton- Blue LED Cheackpoint Commit - Date: 05/01/24

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -1,22 +1,28 @@
 
-// Programmer: LaDanien Shelton  
-// Team Member:  Preston Simmons 
+// Programmer: Preston Simmons
+// Team Member: LaDanien Shelton
 //Bolt ID: 6B78
 //Date: 4.30.2024
 //Program: Maze Competition
 //Program URL:https://edu.sphero.com/program/16788335/edit
 
 async function startProgram() {
-	// Programmer: Preston Simmons - Start Text & Audio Checkpoint Commit - Date
-	   await scrollMatrixText('Start!', {R: 66, G; 56, b: 255}, 90, true)
+	// Programmer: Preston Simmons - Start Text & Audio Checkpoint Commit - Date:04/30/24
+	   await scrollMatrixText('Start!', {R: 66, G: 56, b: 255}, 90, true)
 	   await delay(1)
-	   awati speak("Start", true);
-
-	// Programmer - Blue LED Checkpoint Commit - Date:
+	   await speak("Start", true);
 
 
 
-	//Programmer:           - Sound One Checkpoint Commit - Date:
+	// Programmer:LaDanien Shelton- Blue LED Cheackpoint Commit - Date: 05/01/24
+	
+	setMainLed({ r: 0, g: 0, b: 255});
+	await roll((getHeading() + 0), 60, 2.7);
+   
+}
+
+
+   //Programmer:           - Sound One Checkpoint Commit - Date:
 
 
 
